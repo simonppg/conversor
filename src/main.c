f140243a8c235e7871532f9f6df1e30617598200
victoriact <victoria.cont@gmail.com>
2015-05-11 19:44:15 -0500

    Suma desde origin develop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include<>
 int main(){
+	
+	int num1 = 0, num2 = 0;
+	
 	printf("Conversor");
 	printf("Develop");
 	printf("Developeee");
+	num1 = 2;
+	num2 = 19;
+	print("\n %d  + %d = %d \n", num1, num2, num1+num2);
+	
 	return 0;
 }
