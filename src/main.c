92a905fe4675fe229fe1bf978e5ea0843c6e1c2d
simonppg <simonppg@gmail.com>
2015-05-11 19:41:24 -0500

    primer desde feature

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
-#include<>
+#include <stdio.h>
+
+void miFuncion();
+
 int main(){
 	printf("Conversor");
 	printf("Develop");
 	printf("Developeee");
+	miFuncion();
 	return 0;
+}
+
+void miFuncion()
+{
+	printf("Agregando nuevas caracteristicas");
 }
