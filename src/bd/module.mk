eddc9dc1d765069a5d3439f33a6b4dcc013d1c94
simonppg <simonppg@gmail.com>
2015-05-25 22:06:07 -0500

    compilacion por modulos y dependencias personalizadar terminado

--- src/bd/module.mk
+++ src/bd/module.mk
@@ -1,3 +1,5 @@
-MYSQLFLAGS 	= `mysql_config --cflags --libs`
-obj/bd/bd.o:  CFLAGS+= $(MYSQLFLAGS)
+CURRENT_C		= $(shell find -name "bd.c")
+CURRENT_O 		= $(patsubst ./$(SRCDIR)/%,./$(BUILDDIR)/%,$(CURRENT_C:.$(SRCEXT)=.$(OBJEXT)))
+MYSQLFLAGS 		= `mysql_config --cflags --libs`
+$(CURRENT_O): CFLAGS+= $(MYSQLFLAGS)
 $(TARGET): LIB += $(MYSQLFLAGS)
